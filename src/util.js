50db87f0740f704449abe7c2c4d84b7629cb7db9
Hiraoka <khi@users.osdn.me>
2018-05-29 21:19:34 +0900

    cleaning (module.exports)

--- src/util.js
+++ src/util.js
@@ -1,24 +1,15 @@
 // utilities
 
-const to_i = x => (x | 0)
-const to_f = x => (x - 0)
-const xor = (a, b) => (a && !b) || (!a && b)
-// const sum = a => a.reduce((r,x) => r + x, 0)
-const clone = x => JSON.parse(JSON.stringify(x))
-const flatten = a => [].concat(...a)
-const each_key_value = (h, f) => Object.keys(h).forEach(k => f(k, h[k]))
+const E = module.exports
+
+E.to_i = x => (x | 0)
+E.to_f = x => (x - 0)
+E.xor = (a, b) => (a && !b) || (!a && b)
+// E.sum = a => a.reduce((r,x) => r + x, 0)
+E.clone = x => JSON.parse(JSON.stringify(x))
+E.flatten = a => [].concat(...a)
+E.each_key_value = (h, f) => Object.keys(h).forEach(k => f(k, h[k]))
 
 // seq(3) = [ 0, 1, 2 ], seq(3, 5) = [ 5, 6, 7 ]
-const seq = (n, from) => [...Array(n)].map((_, i) => i + (from || 0))
-const do_ntimes = (n, f) => seq(n).forEach(f)
-
-module.exports = {
-    to_i: to_i,
-    to_f: to_f,
-    xor: xor,
-    clone: clone,
-    flatten: flatten,
-    each_key_value: each_key_value,
-    seq: seq,
-    do_ntimes: do_ntimes,
-}
+E.seq = (n, from) => [...Array(n)].map((_, i) => i + (from || 0))
+E.do_ntimes = (n, f) => E.seq(n).forEach(f)
