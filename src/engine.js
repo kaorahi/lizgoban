417155df2c19e1cf1770e7c746bb67ac953b42cd
Hiraoka <khi@users.osdn.me>
2019-06-19 21:30:45 +0900

    accept startup message of KataGo

--- src/engine.js
+++ src/engine.js
@@ -252,7 +252,8 @@ function create_leelaz () {
         let m, c;
         (m = s.match(/Detecting residual layers.*?([0-9]+) channels.*?([0-9]+) blocks/)) &&
             (network_size_text = `${m[1]}x${m[2]}`);
-        (m = s.match(/Setting max tree size/) && on_ready());
+        // "GTP ready" for KataGo
+        (m = s.match(/(Setting max tree size)|(GTP ready)/) && on_ready());
         (m = s.match(/Weights file is the wrong version/) && on_error());
         (m = s.match(/NN eval=([0-9.]+)/)) && the_nn_eval_reader(to_f(m[1]));
         s.match(/endstate:/) && (current_reader = endstate_reader)
