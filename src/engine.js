d536ca40333efe6f2404da55f1d91a581bb424bb
Hiraoka <khi@users.osdn.me>
2019-06-30 09:12:34 +0900

    Revert "check availability of "undo" command for KataGo v1.1" for #29
    This reverts commit 6aa6b4e0646e3f12cd3d0e1f9604dc771b2a6279.
    It is needless because KataGo has "undo" now.

--- src/engine.js
+++ src/engine.js
@@ -84,7 +84,6 @@ function create_leelaz () {
     let on_ready = () => {
         if (is_ready) {return}; is_ready = true
         send_to_leelaz(`komi ${arg.komi}`)
-        send_to_leelaz('play b a1'); check_supported('undo', 'undo')
         check_supported('minmoves', 'lz-analyze interval 1 minmoves 30')
         check_supported('endstate', 'endstate_map')
         check_supported('kata-analyze', 'kata-analyze interval 1')
@@ -101,11 +100,7 @@ function create_leelaz () {
         const beg = common_header_length(history, leelaz_previous_history)
         const back = leelaz_previous_history.length - beg
         const rest = history.slice(beg)
-        // KataGo v1.1 does not have 'undo'
-        const rewind = () => back <= 0 ? do_nothing() :
-              is_supported('undo') ? do_ntimes(back, undo1) :
-              (leelaz('clear_board'), history.slice(0, beg).forEach(play1))
-        rewind(); rest.forEach(play1)
+        do_ntimes(back, undo1); rest.forEach(play1)
         if (back > 0 || !empty(rest)) {update_move_count(history)}
         leelaz_previous_history = shallow_copy_array(history)
     }
