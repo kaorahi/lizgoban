4b2af31ba3b9951d44d7607e2c58963493c05a5a
Hiraoka <khi@users.osdn.me>
2019-09-14 20:56:00 +0900

    separate corridors for more "natural" area counts

--- src/area.js
+++ src/area.js
@@ -1,52 +1,145 @@
 // clustering & counting of areas
 
+// fix me: inefficient...
+
 require('./util.js').use(); require('./coord.js').use()
 
 const minor_ownership = 0.1
+const narrow_corridor_radius = 3
+const too_small_corridor_cluster_size = 3
+
 const category_spec = [
-    {color: 'white', type: 'major', ownership_range: [- Infinity, - minor_ownership]},
-    {color: 'white', type: 'minor', ownership_range: [- minor_ownership, 0]},
-    {color: 'black', type: 'minor', ownership_range: [0, minor_ownership]},
-    {color: 'black', type: 'major', ownership_range: [minor_ownership, Infinity]},
+    {color: 'black', type: 'major', ownership_range: [minor_ownership, Infinity],
+     corridor_radius: narrow_corridor_radius},
+    {color: 'white', type: 'major', ownership_range: [- Infinity, - minor_ownership],
+     corridor_radius: narrow_corridor_radius},
+    {color: 'black', type: 'minor', ownership_range: [0, Infinity],
+     corridor_radius: 0},
+    {color: 'white', type: 'minor', ownership_range: [- Infinity, 0],
+     corridor_radius: 0},
 ]
 
+//////////////////////////////////////
+// main
+
 function endstate_clusters_for(endstate) {
-    const grid_for = z => ({ownership: z, category: category_for(z), done: false})
-    const grid = aa_map(endstate, grid_for), result = []
-    aa_each(grid, (g, i, j) => g.done || result.push(cluster_from(g, i, j, grid)))
-    return result
+    const grid_for = z => ({ownership: z, done: false})
+    const grid = aa_map(endstate, grid_for)
+    return flatten(category_spec.map((_, cat) => clusters_in_category(cat, grid)))
 }
 
-function category_for(ownership) {
-    const f = ({ownership_range: [a, b]}) => (a <= ownership && ownership < b)
-    return category_spec.findIndex(f)
+function clusters_in_category(category, grid) {
+    const {corridor_radius} = category_spec[category]
+    const region = region_for_category(category, grid)
+    // (1) exclude corridor parts first (by marking "done" on grid)
+    const corridor_clusters =
+          corridor_clusters_in(region, grid, category, corridor_radius)
+    // (2) get clusters in rest parts
+    const normal_clusters = clusters_in_region(region, grid, category)
+    return [...normal_clusters, ...corridor_clusters]
 }
 
-function cluster_from(g, i, j, grid) {
-    const {category} = g, {color, type} = category_spec[category]
-    const state = {ownership_sum: 0.0, i_sum: 0.0, j_sum: 0.0, newcomers: [], category}
+function region_for_category(category, grid) {
+    const region = [], ok = g => !g.done && in_category(category, g.ownership)
+    aa_each(grid, (g, i, j) => ok(g) && region.push([i, j]))
+    return region
+}
+
+function in_category(category, ownership) {
+    const {ownership_range: [a, b]} = category_spec[category]
+    return a <= ownership && ownership < b
+}
+
+//////////////////////////////////////
+// clustering
+
+function clusters_in_region(region, grid, category) {
+    return cleanup_clusters(clusters_with_ijs_in_region(region, grid, category))
+}
+
+function cleanup_clusters(clusters) {
+    // for efficiency
+    const cleanup = c => {delete c.ijs; return c}; return clusters.map(cleanup)
+}
+
+function clusters_with_ijs_in_region(region, grid, category) {
+    return region.map(ij => cluster_from(ij, region, grid, category)).filter(truep)
+}
+
+function cluster_from([i, j], region, grid, category) {
+    if (grid[i][j].done) {return null}
+    const {color, type} = category_spec[category]
+    const state = {ijs: [], newcomers: [], region}
     add_newcomer_maybe([i, j], state, grid)
     while (!empty(state.newcomers)) {search_around(state.newcomers.pop(), state, grid)}
-    const {ownership_sum} = state, center_idx = center_idx_for(state)
-    return {color, type, ownership_sum, center_idx}
+    const {ijs} = state
+    return {color, type, ijs, ...cluster_characteristics(ijs, grid)}
 }
 
 function add_newcomer_maybe(ij, state, grid) {
-    const g = aa_ref(grid, ...ij), ok = g && !g.done && (g.category === state.category)
-    if (!ok) {return}
-    const s = state, [i, j] = ij, {ownership} = g
-    s.newcomers.push(ij); g.done = true
-    s.ownership_sum += ownership; s.i_sum += i * ownership; s.j_sum += j * ownership
+    const g = aa_ref(grid, ...ij)
+    if (!g || g.done || !is_member(ij, state.region)) {return}
+    state.ijs.push(ij); state.newcomers.push(ij); g.done = true
 }
 
 function search_around(ij, state, grid) {
     around_idx(ij).forEach(idx => add_newcomer_maybe(idx, state, grid))
 }
 
-function center_idx_for(state) {
-    const {ownership_sum, i_sum, j_sum} = state
-    return [i_sum, j_sum].map(z => z / ownership_sum)
+function cluster_characteristics(ijs, grid) {
+    const sum = (v, w) => v.map((_, k) => v[k] + w[k]), zero = [0, 0, 0]
+    const f = ([i, j]) => {const ow = grid[i][j].ownership; return [ow, i * ow, j * ow]}
+    const [ownership_sum, i_sum, j_sum] = ijs.map(f).reduce(sum, zero)
+    const center_idx = [i_sum, j_sum].map(z => z / ownership_sum)
+    return {ownership_sum, center_idx}
 }
+
+//////////////////////////////////////
+// separate corridors for "natural" clustering
+
+function corridor_clusters_in(region, grid, category, corridor_radius) {
+    const corridors = corridors_in(region, grid, corridor_radius)
+    const clusters = clusters_with_ijs_in_region(corridors, grid, category)
+    // cancel annoying small clusters
+    const acceptable = c => c.ijs.length > too_small_corridor_cluster_size
+    const inacceptable = c => !acceptable(c)
+    const acceptable_clusters = clusters.filter(acceptable)
+    const inacceptable_clusters = clusters.filter(inacceptable)
+    const cancel = c => c.ijs.forEach(([i, j]) => (grid[i][j].done = false))
+    inacceptable_clusters.forEach(cancel)
+    return cleanup_clusters(acceptable_clusters)
+}
+
+function corridors_in(region, grid, corridor_radius) {
+    const in_board = ([i, j]) => !!aa_ref(grid, i, j)
+    let core = region
+    do_ntimes(corridor_radius, () => (core = erosion(core, in_board)))
+    // "* 2" for recovering corners of rectangles
+    do_ntimes(corridor_radius * 2, () => (core = dilation(core, region)))
+    return region.filter(ij => !is_member(ij, core))
+}
+
+function erosion(region, in_board) {
+    const is_not_member = idx => in_board(idx) && !is_member(idx, region)
+    const is_inside = ij => !find_around(ij, is_not_member)
+    return region.filter(is_inside)
+}
+
+function dilation(region, limit_region) {
+    const is_in_region = idx => is_member(idx, region)
+    const is_in_closure = ij => is_in_region(ij) || find_around(ij, is_in_region)
+    return limit_region.filter(is_in_closure)
+}
+
+function find_around(idx, pred) {return around_idx(idx).find(pred)}
+
+function is_member(idx, region) {
+    const eq = ([i1, j1], [i2, j2]) => (i1 === i2 && j1 === j2)
+    return region.find(ij => eq(idx, ij))
+}
+
+//////////////////////////////////////
+// exports
 
 module.exports = {
     endstate_clusters_for,
