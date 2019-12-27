kaorahi/lizgoban

commit 3d44496e4f595de277e3f2f4ae4fe7cc14a7b278

cleaning (simplify load_weight)

src/main.js
@@ -356,9 +356,8 @@ function menu_template(win) {
         item('Save SGF...', 'CmdOrCtrl+S', save_sgf, true),
         sep,
         item('Reset', 'CmdOrCtrl+R', restart),
-        lz_white ?
-            item('Load weights for black', 'Shift+L', load_leelaz_for_black) :
-            item('Load network weights', 'Shift+L', load_weight),
+        item(lz_white ? 'Load weights for black' : 'Load network weights',
+             'Shift+L', load_leelaz_for_black),
         sep,
         item('Close', undefined, (this_item, win) => win.close()),
         item('Quit', undefined, app.quit),
@@ -475,8 +474,9 @@ function apply_option_preset(rule, win) {
     empty_board && !game.is_empty() && new_empty_board()
     board_type && set_board_type(board_type, win)
     need_restart && merge_option_and_restart({leelaz_command, leelaz_args})
-    weight_file && load(AI.load_leelaz_for_black, weight_file)
-    weight_file_for_white ? load(AI.load_leelaz_for_white, weight_file_for_white) :
+    // backward compatibility for obsolete "weight_file" and "weight_file_for_white"
+    weight_file && load_weight_file(weight_file)
+    weight_file_for_white ? load_weight_file(weight_file_for_white) :
         unload_leelaz_for_white()
     engine_for_white && AI.set_engine_for_white(engine_for_white)
     resume()
@@ -965,15 +965,19 @@ function availability() {
 
 // load weight file
 let previous_weight_file = null
-function load_weight() {return load_weight_file(select_weight_file())}
-function load_weight_file(weight_file) {
-    const current_weight_file = AI.leelaz_weight_file()
+function load_weight(white_p) {return load_weight_file(select_weight_file(), white_p)}
+function load_weight_file(weight_file, white_p) {
     if (!weight_file) {return false}
-    weight_file !== current_weight_file &&
+    const current_weight_file = AI.leelaz_weight_file(white_p)
+    weight_file !== current_weight_file && !white_p &&
         (previous_weight_file = current_weight_file)
-    AI.restart(leelaz_start_args(weight_file), true)
+    AI.load_weight_file(weight_file, white_p)
+    update_ponder_and_ui()
     return weight_file
 }
+function load_leelaz_for_black() {load_weight()}
+function load_leelaz_for_white() {load_weight(true)}
+
 function select_weight_file() {
     const message = 'Select weight file for leela zero'
     return select_files(message, option.weight_dir)[0]
@@ -1029,12 +1033,6 @@ function on_ready() {
     // This interferes starting-up sequence of another leelaz in engine.js.
     switch_to_nth_sequence(sequence_cursor); stop_auto(); update_state()
 }
-
-/////////////////////////////////////////////////
-// another leelaz for white
-
-function load_leelaz_for_black() {AI.load_leelaz_for_black(load_weight)}
-function load_leelaz_for_white() {AI.load_leelaz_for_white(load_weight)}
 
 /////////////////////////////////////////////////
 // SGF

src/ai.js
@@ -41,7 +41,10 @@ function all_start_args() {
     const f = lz => lz && lz.start_args()
     return {black: f(leelaz_for_black), white: f(leelaz_for_white)}
 }
-function leelaz_weight_file() {return leelaz.get_weight_file()}
+function leelaz_weight_file(white_p) {
+    const lz = white_p ? leelaz_for_white : leelaz_for_black
+    return lz && lz.get_weight_file()
+}
 
 function each_leelaz(f, for_black_and_white_only) {
     [leelaz_for_black, leelaz_for_white,
@@ -79,15 +82,6 @@ function swap_leelaz_for_black_and_white() {
 function switch_to_random_leelaz(percent) {
     switch_leelaz(xor(is_bturn(), Math.random() < percent / 100))
 }
-function load_leelaz_for_black(load_weight) {
-    with_temporary_leelaz(leelaz_for_black, load_weight)
-}
-function load_leelaz_for_white(load_weight) {
-    const proc = () => {
-        load_weight() || (leelaz_for_white.kill(), (leelaz_for_white = null))
-    }
-    with_temporary_leelaz(leelaz_for_white = create_leelaz(), proc)
-}
 function set_engine_for_white(command_args) {
     const [leelaz_command, ...leelaz_args] = command_args
     const start_args = {...leelaz_for_black.start_args(), leelaz_command, leelaz_args}
@@ -104,13 +98,17 @@ function switch_leelaz(bturn) {
     return switch_to_another_leelaz((bturn === undefined ? is_bturn() : bturn) ?
                                     leelaz_for_black : leelaz_for_white)
 }
+function load_weight_file(weight_file, white_p) {
+    set_pondering(false)
+    const lz = white_p ? (leelaz_for_white || (leelaz_for_white = create_leelaz()))
+          : leelaz_for_black
+    const sa = lz.start_args() || leelaz_for_black.start_args()
+    lz.restart({...sa, weight_file})
+    switch_leelaz()
+}
 
 // internal
 
-function with_temporary_leelaz(leelaz_for_black_or_white, proc) {
-    leelaz = leelaz_for_black_or_white; proc()
-    leelaz = leelaz_for_black; switch_leelaz()
-}
 function switch_to_another_leelaz(next_leelaz) {
     return next_leelaz && next_leelaz !== leelaz && (leelaz = next_leelaz)
 }
@@ -151,7 +149,7 @@ module.exports = {
     initialize, set_board,
     start_leelaz, update_leelaz, kill_all_leelaz, set_pondering, all_start_args,
     leelaz_for_white_p, swap_leelaz_for_black_and_white, switch_leelaz,
-    switch_to_random_leelaz, load_leelaz_for_black, load_leelaz_for_white,
+    switch_to_random_leelaz, load_weight_file,
     unload_leelaz_for_white, leelaz_weight_file, restart,
     set_engine_for_white, support_komi_p, set_engine_komi, get_engine_komi,
     ...aa2hash(exported_from_leelaz.map(key =>

commit c51887e03996ba4d29f3b8e7022fd68639d9afea

fix: weight_file_for_white was broken from 3d44496

src/main.js
@@ -590,7 +590,7 @@ function apply_preset(rule, win) {
     need_restart && merge_option_and_restart({leelaz_command, leelaz_args})
     // backward compatibility for obsolete "weight_file" and "weight_file_for_white"
     weight_file && load_weight_file(weight_file)
-    weight_file_for_white ? load_weight_file(weight_file_for_white) :
+    weight_file_for_white ? load_weight_file(weight_file_for_white, true) :
         unload_leelaz_for_white()
     engine_for_white && AI.set_engine_for_white(engine_for_white)
     set_board(game); resume()