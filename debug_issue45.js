6d3e3470a300c0c0a8b8cf6ddbc6851b908284bb
Hiraoka <khi@users.osdn.me>
2020-03-07 20:17:56 +0900

    add idle_loop etc. to debug_issue45.js

--- debug_issue45.js
+++ debug_issue45.js
@@ -1,25 +1,60 @@
+// usage: node this_file
+
+/////////////////////////
+// setting
+
 const engine_command = '/foo/bar/katago'
 const engine_args = ['gtp',
                      '-model', '/foo/bar/baz.bin.gz',
                      '-config', '/foo/bar/qux.cfg']
-const opt = {}
 
-const gtp_commands = 'lz-analyze interval 20\n'
+const gtp_commands = 'kata-analyze interval 20 ownership true\n'
+
+const use_each_line = true
+
+const overhead_centisec = 20
 
 /////////////////////////
+// util
 
+function log(s) {console.log(`${(new Date()).toJSON()} ${s}`)}
+
+const empty = a => !a || (a.length === 0)
+const each_line = (f) => {
+    let buf = ''
+    return stream => {
+        const raw_str = stream.toString()
+        if (!use_each_line) {f(raw_str); return}
+        log(`each_line: received ${raw_str.length} chars`)
+        const a = raw_str.split(/\r?\n/), rest = a.pop()
+        log(`each_line: ${a.length} lines + ${rest.length} chars`)
+        !empty(a) && (a[0] = buf + a[0], buf = '', a.forEach(f))
+        buf += rest
+    }
+}
+
+function idle_loop(millisec) {
+    for (const start = Date.now(); Date.now() - start < millisec; ) {}
+}
+
+/////////////////////////
+// main
+
+const opt = {}
 const engine_process = require('child_process').spawn(engine_command, engine_args, opt)
 
-function stdout_reader(stream) {
-    const s = stream.toString()
-    console.log(`${(new Date()).toJSON()} ${s.slice(0, 30)}... (${s.length} chars)`)
+function stdout_reader(s) {
+    log(`${s.slice(0, 30)}... (${s.length} chars)`)
+    log(`[begin idle loop]`)
+    idle_loop(overhead_centisec * 10)
+    log(`[end idle loop]`)
 }
 function stderr_reader(stream) {
     const s = stream.toString()
-    console.log(s)
+    log(s)
     s.match(/(Setting max tree size)|(GTP ready)/) &&
         engine_process.stdin.write(gtp_commands)
 }
 
-engine_process.stdout.on('data', stdout_reader)
+engine_process.stdout.on('data', each_line(stdout_reader))
 engine_process.stderr.on('data', stderr_reader)
