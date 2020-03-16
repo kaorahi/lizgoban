kaorahi/lizgoban

commit 3c0eef379b0ef26dbfecff055eaab84172bdbb74

fix lags for too small analyze_interval_centisec (ref. #45)

src/renderer.js
@@ -100,7 +100,18 @@ function main(channel, ...args) {ipc.send(channel, ...args)}
 /////////////////////////////////////////////////
 // from main
 
-ipc.on('render', (e, h, is_board_changed) => {
+let latest_rendering_request = null
+ipc.on('render', (...args) => {
+    // skip too frequent requests so that we can avoid serious lags
+    const render_latest = () => {
+        render_now(...latest_rendering_request); latest_rendering_request = null
+    }
+    const idle = !latest_rendering_request; latest_rendering_request = args
+    idle && setTimeout(render_latest)  // executed in the next event cycle
+    // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout
+})
+
+function render_now(e, h, is_board_changed) {
     // for smooth reaction or readable variation display
     if (showing_until() && !is_board_changed) {return}
     keep_selected_variation_maybe(h.suggest)
@@ -111,7 +122,7 @@ ipc.on('render', (e, h, is_board_changed) => {
     setq('#comment', R.comment)
     D.update_winrate_trail()
     update_goban()
-})
+}
 
 ipc.on('update_ui', (e, win_prop, availability, ui_only) => {
     R.pausing = availability.resume

commit b88bb943c71433ce43cf2866d5cc05998c37ea39

fix: "smoother c+hover" (dae574cc8) was broken after 3c0eef37

src/renderer.js
@@ -105,8 +105,11 @@ function main(channel, ...args) {ipc.send(channel, ...args)}
 
 let latest_rendering_request = null
 ipc.on('render', (...args) => {
+    const [e, h, is_board_changed] = args
+    // for smooth reaction or readable variation display
+    if (truep(showing_until()) && !is_board_changed) {return}
     // renderer state must be updated before update_ui is called
-    const [e, h, is_board_changed] = args; merge(R, h)
+    merge(R, h)
     // skip too frequent requests so that we can avoid serious lags
     const render_latest = () => {
         render_now(...latest_rendering_request); latest_rendering_request = null
@@ -117,8 +120,6 @@ ipc.on('render', (...args) => {
 })
 
 function render_now(e, h, is_board_changed) {
-    // for smooth reaction or readable variation display
-    if (truep(showing_until()) && !is_board_changed) {return}
     keep_selected_variation_maybe(h.suggest)
     set_board_size(R.bsize)
     setq('#move_count', D.movenum())