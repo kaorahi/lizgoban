0b34f992d242fe05a45eab31eb7e190009ca20f7
Hiraoka <khi@users.osdn.me>
2020-03-16 21:06:49 +0900

    fix wrong "mistake marks" in ko fights

--- src/powered_goban.js
+++ src/powered_goban.js
@@ -253,7 +253,7 @@ function winrate_from_game(engine_id) {
                 score_loss[opponent_letter] += transferred
                 // clean me: record gain as a side effect
                 !engine_id && s > 0 && truep(score_without_komi_at(s - 1)) &&
-                    (merge(aa_ref(R.stones, ...move2idx(cur.move)) || {}, {gain}),
+                    (s <= game.move_count && merge(aa_ref(R.stones, ...move2idx(cur.move)) || {}, {gain}),
                      merge(cur, {gain}))
             }
             prev_score = score_without_komi
