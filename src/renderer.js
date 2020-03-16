kaorahi/lizgoban

commit 557a5c29cfd736ae6f535989690a9d908d7bcf2d

fix: title bar was not updated (bug in 3c0eef37)

src/renderer.js
@@ -103,6 +103,8 @@ function main(channel, ...args) {ipc.send(channel, ...args)}
 
 let latest_rendering_request = null
 ipc.on('render', (...args) => {
+    // renderer state must be updated before update_ui is called
+    const [e, h, is_board_changed] = args; merge(R, h)
     // skip too frequent requests so that we can avoid serious lags
     const render_latest = () => {
         render_now(...latest_rendering_request); latest_rendering_request = null
@@ -116,7 +118,6 @@ function render_now(e, h, is_board_changed) {
     // for smooth reaction or readable variation display
     if (showing_until() && !is_board_changed) {return}
     keep_selected_variation_maybe(h.suggest)
-    merge(R, h)
     set_board_size(R.bsize)
     setq('#move_count', D.movenum())
     setq('#history_length', ' (' + D.max_movenum() + ')')

commit 71eb7362b0a26997cced43879b934ebe56bba87d

fix: "readable variations" (3132e1de, b2575993) was broken after 557a5c29

src/renderer.js
@@ -108,6 +108,7 @@ ipc.on('render', (...args) => {
     const [e, h, is_board_changed] = args
     // for smooth reaction or readable variation display
     if (truep(showing_until()) && !is_board_changed) {return}
+    keep_selected_variation_maybe(h.suggest)
     // renderer state must be updated before update_ui is called
     merge(R, h)
     // skip too frequent requests so that we can avoid serious lags
@@ -120,7 +121,6 @@ ipc.on('render', (...args) => {
 })
 
 function render_now(e, h, is_board_changed) {
-    keep_selected_variation_maybe(h.suggest)
     set_board_size(R.bsize)
     setq('#move_count', D.movenum())
     setq('#history_length', ' (' + D.max_movenum() + ')')