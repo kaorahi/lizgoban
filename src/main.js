ecd42f55e17d9fc4c1048e886a728f77e9a7d0d6
Hiraoka <khi@users.osdn.me>
2020-03-25 21:31:48 +0900

    move sgf_file to top in info dialog for convenience

--- src/main.js
+++ src/main.js
@@ -909,9 +909,8 @@ function info_text() {
     const lz = AI.leelaz_for_white_p() ?
           (f("engine (black)", sa.black) + f("engine (white)", sa.white)) :
           f("engine", sa.black)
-    const message = lz +
-          f("sgf file", game.sgf_file) +
-          f("sgf", game.sgf_str)
+    const {sgf_file} = game
+    const message = f("sgf file", game.sgf_file) + lz + f("sgf", game.sgf_str)
     return message
 }
 function set_game_info(player_black, player_white, komi, sgf_gorule, gorule, comment) {
