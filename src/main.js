kaorahi/lizgoban

commit 427f2ebd3b22cfca530728dd15497be8a79eb8bc

fix unintended rewinding after loading of games when engine is not ready

src/main.js
@@ -763,6 +763,7 @@ function toggle_auto_analyze(visits) {
         start_auto_analyze(visits)
 }
 function start_auto_analyze(visits, steps, on_finish) {
+    if (!AI.engine_info().is_ready) {return}
     set_auto_analysis_signed_visits(visits); auto_analysis_steps = steps || 1
     on_auto_analyze_finished = on_finish || pause
     rewind_maybe(); resume()

commit 04c9bec6f5bce0b01d8fa7adb8c4fa2bbfdf2d2c

fix: quick overview was broken in 427f2ebd3

src/main.js
@@ -764,7 +764,7 @@ function toggle_auto_analyze(visits) {
         start_auto_analyze(visits)
 }
 function start_auto_analyze(visits, steps, on_finish) {
-    if (!AI.engine_info().is_ready) {return}
+    if (!AI.engine_info().current.is_ready) {return}
     set_auto_analysis_signed_visits(visits); auto_analysis_steps = steps || 1
     on_auto_analyze_finished = on_finish || pause
     rewind_maybe(); resume()