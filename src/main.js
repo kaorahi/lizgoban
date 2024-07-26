2bcfbee70aed2d1a0e7e7060bb649113f12492e3
Hiraoka <kakkokakko@gmail.com>
2024-07-26 21:59:42 +0900

    add "auto_play_weaken_for_b/w" to preset in config.json (undocumented)

--- src/main.js
+++ src/main.js
@@ -861,6 +861,7 @@ function apply_preset(rule, win) {
     stored.forEach(key => (rule[key] !== undefined) && set_stored(key, rule[key]))
     const {
         empty_board, board_type, match, rules, handicap, komi,
+        auto_play_weaken_for_b, auto_play_weaken_for_w,
     } = rule
     const bsize = rule.board_size
     const new_board_p = (empty_board && !game.is_empty()) ||
@@ -871,6 +872,9 @@ function apply_preset(rule, win) {
     truep(komi) && set_komi(komi)
     board_type && set_board_type(board_type, win)
     match && start_match(win, ...(is_a(match, 'object') ? match : [to_i(match)]))
+    const set_weaken = (bw, val) => truep(val) && set_auto_play_weaken_for_bw(bw, val)
+    set_weaken('black', auto_play_weaken_for_b)
+    set_weaken('white', auto_play_weaken_for_w)
     const is_engine_updated = update_engines_by_preset(rule)
     AI.backup(); is_engine_updated && resume()
 }
