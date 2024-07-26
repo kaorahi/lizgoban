kaorahi/lizgoban

commit c2c17a0b0a95d350791da7bed4c9bc1505205fe3

fix: "0" key for weakened engine was broken

src/engine.js
@@ -205,13 +205,16 @@ function create_leelaz () {
           `${base_engine_id}-${gorule}-${komi}${aggressive}${analysis_region}`
     const peek_value = (move, cont) => {
         if (!is_supported('lz-setoption')) {return false}
-        the_nn_eval_reader =
-            value => {the_nn_eval_reader = do_nothing; cont(value); update()}
-        leelaz(join_commands('lz-setoption name visits value 1',
-                             `play ${bturn ? 'b' : 'w'} ${move}`,
-                             'lz-analyze interval 0',
-                             'lz-setoption name visits value 0', 'undo'))
-        return true
+        const do1 = () =>
+              leelaz(join_commands('lz-setoption name visits value 1',
+                                   `play ${bturn ? 'b' : 'w'} ${move}`,
+                                   'lz-analyze interval 0'), do2)
+        const do2 = () => {
+            the_nn_eval_reader =
+                value => {the_nn_eval_reader = do_nothing; cont(value); update()}
+            leelaz(join_commands('lz-setoption name visits value 0', 'undo'))
+        }
+        do1(); return true
     }
 
     // aggressive

commit 75e2746a6f6b4694c4ea593e133a1ec47780e2b8

delete useless "command filter" (broken by c2c17a0b0a. cf. 919da47e32)

src/engine.js
@@ -567,7 +567,7 @@ function create_leelaz () {
         // duplicated endstate
         endstate_command_p(task) && remove(endstate_command_p)
         // obsolete endstate / peek
-        changer_command_p(task) && [raw_nn_command_p, endstate_command_p, peek_command_p].forEach(remove)
+        changer_command_p(task) && [raw_nn_command_p, endstate_command_p].forEach(remove)
         clear_command_p(task) && remove(changer_command_p)
         command_queue.push(task); send_from_queue()
     }
@@ -620,7 +620,6 @@ function create_leelaz () {
     const pondering_command_p = command_matcher(/(lz|kata)-analyze/)
     const raw_nn_command_p = command_matcher(/kata-raw(-human)?-nn/)
     const endstate_command_p = command_matcher(/^endstate_map/)
-    const peek_command_p = command_matcher(/play.*undo/)
     const changer_command_p = command_matcher(/play|undo|clear_board|set_position|set_free_handicap/)
     const clear_command_p = command_matcher(/clear_board|set_position/)
     const dummy_command_p = command_matcher(/lizgoban/)