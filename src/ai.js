3e196bc724c50dce8ee6475ae59dc127073da048
Hiraoka <kakkokakko@gmail.com>
2025-03-15 19:41:03 +0900

    cleaning (newline)

--- src/ai.js
+++ src/ai.js
@@ -284,7 +284,11 @@ function engine_ids() {
     return engines.map(lz => lz && lz.engine_id()).filter(truep)
 }
 
-const exported_from_leelaz = ['send_to_leelaz', 'peek_value', 'peek_kata_raw_human_nn', 'get_komi', 'is_supported', 'analyze_move']
+const exported_from_leelaz = [
+    'send_to_leelaz',
+    'peek_value', 'peek_kata_raw_human_nn',
+    'get_komi', 'is_supported', 'analyze_move',
+]
 
 module.exports = {
     // main.js only
